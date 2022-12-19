e484aba1ccafc2878f9af27a2d6e8144502ad8a7
Aleks Rūtiņš <aleks@rutins.com>
2022-12-19 18:31:21 -0500

    initial commit

---
+++ init.el
@@ -0,0 +1,35 @@
+(eval-when-compile (require 'use-package))
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+(use-package elcord
+  :config
+  (elcord-mode))
+(use-package lsp-mode)
+(use-package lsp-ui)
+(use-package treemacs)
+(use-package geiser-guile)
+(use-package atom-one-dark-theme
+  :config
+  (load-theme 'atom-one-dark t))
+(use-package paredit)
+(use-package magit)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
